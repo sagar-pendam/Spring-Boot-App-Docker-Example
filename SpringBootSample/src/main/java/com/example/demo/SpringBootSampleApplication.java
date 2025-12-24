cd82d45a50c8d3e2fd5e71d903381e594d6dd6ef
pendamsagar <pendamsagar3@gmail.com>
2025-12-24 22:53:07 +0530

    dsfdsf
    Change-Id: Id03859055d45ed0b4af94ba0322b526223469d09

---
+++ SpringBootSample/src/main/java/com/example/demo/SpringBootSampleApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSampleApplication.class, args);
+		System.out.println("Welcome to Docker App");
+	}
+
+}
